fe6bc47a0562d07f64d0b905ac18d1814a9f83e5
Lolade <loladeilori@cogartstudio.com>
2022-03-16 02:37:03 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UseRefExample1 from "./components/UseRefExample1";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <UseRefExample1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UseRefExample1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UseRefExample1() {
+  return (
+    <div>UseRefExample1</div>
+  )
+}
+
+export default UseRefExample1
