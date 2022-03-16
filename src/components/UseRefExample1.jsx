b206bf0ecb25a3415605ef13527d06f96749abbb
Lolade <loladeilori@cogartstudio.com>
2022-03-16 03:29:59 +0100

    using useRef as a reference to various DOM elements

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useRef} from 'react'
 
 function UseRefExample1() {
+    const inputRef = useRef()
+    const paraRef = useRef()
+
+    const onSubmit = e => {
+        e.preventDefault()
+        console.log(inputRef.current.value)
+        inputRef.current.value='Come again'
+        inputRef.current.style.background='red'
+        paraRef.current.innerText='Thank you'
+    }
+
   return (
-    <div>UseRefExample1</div>
+    <div>
+        <form onSubmit={onSubmit}>
+            <label htmlFor="name">Name</label>
+            <input type="text" ref={inputRef} className='form-control mb-2' id='name' />
+            <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
+
+        <p onClick={() => inputRef.current.focus()} ref={paraRef}></p>
+    </div>
+
   )
 }
 
